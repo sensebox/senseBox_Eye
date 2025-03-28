fc539b24a46d67d9164d552ee54521a8a8a9663e
Paula Scharf <paulascharf@gmx.de>
2025-03-28 15:28:29 +0100

    add analog example

---
+++ examples/GPIO/analog/analog.ino
@@ -0,0 +1,15 @@
+#define gpio_pin 3
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(gpio_pin,INPUT);
+  analogReadResolution(13);
+  analogSetAttenuation(ADC_11db);
+  while (!Serial) ;
+}
+
+void loop() {
+  int analogValue = analogReadMilliVolts(gpio_pin);
+  Serial.println(analogValue);
+  delay(100);
+}
