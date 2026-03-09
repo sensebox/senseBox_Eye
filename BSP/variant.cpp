85a0f63d5e00910e07fd0507cac60c83ef4e7851
Benjamin Karic <b_kari02@uni-muenster.de>
2026-03-09 16:07:03 +0100

    update bsp files and add radiolib lora example for sx1276 chip

--- BSP/variant.cpp
+++ BSP/variant.cpp
@@ -1,81 +1,39 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2021 Ha Thach (tinyusb.org) for Adafruit Industries
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
 #include "esp32-hal-gpio.h"
 #include "pins_arduino.h"
-#include "driver/rmt.h"
+#include "esp_log.h"
+#include "esp_partition.h"
+#include "esp_system.h"
+#include "esp_ota_ops.h"
 
 extern "C" {
 
-// Initialize variant/board, called before setup()
-void initVariant(void) {
-  // *************** blink LED ***************
-  rmt_config_t config = RMT_DEFAULT_CONFIG_TX((gpio_num_t)PIN_LED, RMT_CHANNEL_0);
-  config.clk_div = 2;
-  rmt_config(&config);
-  rmt_driver_install(RMT_CHANNEL_0, 0, 0);
+void blinkLED(uint8_t r, uint8_t g, uint8_t b) {
+  rgbLedWrite(PIN_LED, r, g, b);
+  delay(20);
+  rgbLedWrite(PIN_LED, 0x00, 0x00, 0x00);  // off
+}
 
-  rmt_item32_t rmt_green[24] = {
-    // G
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {34, 1, 34, 0},{14, 1, 14, 0},
-    {34, 1, 34, 0},{14, 1, 34, 0},
-    // R
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 14, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    // B
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 14, 0},{34, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0}
-  };
-  rmt_write_items(RMT_CHANNEL_0, rmt_green, 24, true);
-  rmt_wait_tx_done(RMT_CHANNEL_0, pdMS_TO_TICKS(10));
-  delay(80);
-  rmt_item32_t rmt_black[24] = {
-    // G
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    // R
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    // B
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0},
-    {14, 1, 34, 0},{14, 1, 34, 0}
-  };
-  rmt_write_items(RMT_CHANNEL_0, rmt_black, 24, true);
-  rmt_wait_tx_done(RMT_CHANNEL_0, pdMS_TO_TICKS(10));
-  // **************************************
-  // TODO: do we want to power the SD card by default?
+void initVariant(void) {
+  // define button pin
+  pinMode(47, INPUT_PULLUP);
+
+  // Check if button is pressed
+  if (digitalRead(47) == LOW) {
+    // When the button is pressed and then released, boot into the OTA1 partition
+    const esp_partition_t *ota1_partition = esp_partition_find_first(ESP_PARTITION_TYPE_APP, ESP_PARTITION_SUBTYPE_APP_OTA_1, NULL);
+
+    if (ota1_partition) {
+      esp_err_t err = esp_ota_set_boot_partition(ota1_partition);
+      if (err == ESP_OK) {
+        blinkLED(0x00, 0x00, 0x10);  // blue
+        esp_restart();               // restart, to boot OTA1 partition
+      } else {
+        blinkLED(0x10, 0x00, 0x00);  // red
+        ESP_LOGE("OTA", "Error setting OTA1 partition: %s", esp_err_to_name(err));
+      }
+    }
+  } else {
+    blinkLED(0x00, 0x10, 0x00);  // green
+  }
 }
 }
